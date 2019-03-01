8c6ff6f3b1df1be6bc4e5bcaf23634838d01e71b
jscr189 <jscr189@gmail.com>
2019-03-01 11:37:31 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+      <div style="text-align:center">
+        <h1>
+          Hello!!! Welcome to {{ title }}!
+        </h1>
+      </div>
+  `
 })
 export class AppComponent {
-  title = 'sasApp';
+  title = 'Selenium Automation Suite';
 }
